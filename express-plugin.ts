7116f77155df73ea5985fde8754461b55f5b29cc
Alon <alon.glater@protonmail.com>
2023-05-29 16:09:01 +0300

    Comments

--- express-plugin.ts
+++ express-plugin.ts
@@ -1,8 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* 
+  Credit for the following plugin configuration goes to:
+  https://noam.hashnode.dev/using-vite-to-serve-and-hot-reload-react-app-express-api-together
+*/
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export default function express(path: string) {
   return {
-    name: "vite3-plugin-express",
+    name: "express-plugin",
     configureServer: async (server: any) => {
       server.middlewares.use(async (req: any, res: any, next: any) => {
         process.env["VITE"] = "true";
